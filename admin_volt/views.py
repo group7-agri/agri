88f2554652f6d7043c79add95f93d0088a2b9847
aristodush <aristodush@gmail.com>
2023-01-20 16:50:32 +0200

    "YES"

--- admin_volt/views.py
+++ admin_volt/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from products.models import Product
+
+def customIndex(request):
+    product = Product.objects.all().count()
+    context = {'product': product, 
+                'message': "Am trying"
+    }
+
+
+
+    return render(request, 'admin/customs/custom.html', context)
